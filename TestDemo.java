3fbf9896910781a449325c888de1a731997fe434
Guan-Yuan Wang <uuuu2552@gmail.com>
2020-08-12 18:30:30 +0800

    Add files via upload

---
+++ TestDemo.java
@@ -0,0 +1,15 @@
+package yuan;
+
+public class TestDemo {
+	
+	public static String gender;
+	
+	public static void main(String args[]) {
+		String name = "Guan-Yuan";
+		int age = 24;
+		gender = "male";
+		
+		System.out.println("MY name is " + name + ", I'm " + age + " years old." + "(" + gender + ")");
+	}
+}
+
