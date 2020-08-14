e2479f9df9a012e3e51a1737caa4a1a0795943c7
Guan-Yuan Wang <uuuu2552@gmail.com>
2020-08-14 13:08:11 +0800

    Practice Task
    4-4

---
+++ char_boolean.java
@@ -0,0 +1,17 @@
+package yuan;
+
+public class char_boolean {
+	
+	public static void main(String args[]) {
+		
+		char c = 94; // ASCII CODE
+		
+		System.out.println(c);
+		
+		boolean b = true;
+		
+		System.out.println(b);
+		
+	}
+
+}
