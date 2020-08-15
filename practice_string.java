5dfbcd2c7f99dd66d840cdda3217446d70e76fef
Guan-Yuan Wang <uuuu2552@gmail.com>
2020-08-15 21:28:49 +0800

    Practice Task
    5-2

---
+++ practice_string.java
@@ -0,0 +1,18 @@
+package yuan;
+
+public class practice_string {
+	
+	public static void main(String args[]) {
+		
+		char[] c = {'a', 'b', 'c'};
+		System.out.println(c);
+		
+		String str = new String(c);
+		System.out.println(str);
+		
+		String str2 = "abc";
+		System.out.println(str2.charAt(1));
+		
+	}
+
+}
