9f1063756647215cbf4e87a58674538ecff2cd33
Guan-Yuan Wang <uuuu2552@gmail.com>
2020-08-16 11:32:04 +0800

    Practice Task
    5-7

---
+++ endsWith_startsWith.java
@@ -0,0 +1,17 @@
+package yuan;
+
+public class endsWith_startsWith {
+	
+	public static void main(String[] args) {
+		
+		String str = "abc";
+		
+		//startsWith
+		System.out.println(str.startsWith("b"));
+		
+		//endsWith
+		System.out.println(str.endsWith("c"));
+		
+	}
+
+}
