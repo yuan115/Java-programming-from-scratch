a962d758d65b5ec810e4212a3b5a79c735423d56
Guan-Yuan Wang <uuuu2552@gmail.com>
2020-08-18 00:46:05 +0800

    Practice Task
    7-3

---
+++ Switch.java
@@ -0,0 +1,21 @@
+package yuan;
+
+public class Switch {
+	
+	public static void main(String args[]) {
+		char c = 'd';
+		
+		switch(c) {
+			case 'a':
+				System.out.println("a");
+				break;
+			case 'b':
+				System.out.println("b");
+				break;
+			default:
+				System.out.println("default");
+		
+		}
+	}
+
+}
