b11a613a44c3ad3d498b77b96d1abd30678652dc
Guan-Yuan Wang <uuuu2552@gmail.com>
2020-08-18 01:18:47 +0800

    Practice Task
    7-4

---
+++ While.java
@@ -0,0 +1,24 @@
+package yuan;
+
+public class While {
+
+	public static void main(String args[]) {
+		// while
+		int i = 1;
+		
+		while(i < 10) {
+			System.out.print(i);
+			i ++;
+		}
+		
+		System.out.println("");
+		
+		// do-while
+		int j = 1;
+		do {
+			System.out.print(j);
+			j ++;
+		} while (j < 10);
+		
+	}
+}
