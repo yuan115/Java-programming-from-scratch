9a69a0cc320090c902b3e9e85c3c5cdc19a0efc5
Guan-Yuan Wang <uuuu2552@gmail.com>
2020-08-18 00:37:09 +0800

    Practice Task
    7-2

---
+++ if_else.java
@@ -0,0 +1,17 @@
+package yuan;
+
+public class if_else {
+
+	public static void main(String args[]) {
+		int x = -2;
+		
+		if (x > 0) {
+			System.out.println("This number is greater than 0.");
+		} else if (x == 0) {
+			System.out.println("This number is equal to 0.");
+		} else {
+			System.out.println("This number is less than 0.");
+		}
+		
+	}
+}
